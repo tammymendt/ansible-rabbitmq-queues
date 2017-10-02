8bc37601478d98d6b7bd82a26b65690390ea6b7b
Tamara Mendt <tammymendt@gmail.com>
2017-10-02 18:16:36 +0200

    fixed bug in tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,23 +8,23 @@ ansible-playbook -i localhost, play.yml -e @vars_1.yml
 echo "\nTest for rabbit state"
 docker exec rabbit-mgmt rabbitmqctl list_exchanges | egrep -v "^amq\." | wc -l | egrep "^\s*3$"
 docker exec rabbit-mgmt rabbitmqctl list_exchanges | egrep "$EXCHANGE_NAME\s+direct"
-echo "\nExchanges created successfully"
+echo "Exchanges created successfully\n"
 
 docker exec rabbit-mgmt rabbitmqctl list_queues | egrep "$QUEUE_NAME\s+"
 docker exec rabbit-mgmt rabbitmqctl list_queues |  wc -l | egrep "^\s*2$"
-echo "\nQueues created successfully"
+echo "Queues created successfully\n"
 
 docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "$EXCHANGE_NAME\s+exchange\s+$QUEUE_NAME\s+queue\s+\*\.labels"
 docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "$EXCHANGE_NAME\s+exchange\s+$QUEUE_NAME\s+queue\s+us\.\*"
 docker exec rabbit-mgmt rabbitmqctl list_bindings |  wc -l | egrep "^\s*4$"
-echo "\nBindings created successfully"
+echo "Bindings created successfully\n"
 
 echo "\nExecuting playbook with second set of variables. Remove bindings"
 ansible-playbook -i localhost, play.yml -e @vars_2.yml
 docker exec rabbit-mgmt rabbitmqctl list_bindings |  wc -l | egrep "^\s*2$"
 echo "\nBindings removed successfully"
 
-echo "\nExecuting playbook with second set of variables. Remove exchanges"
+echo "\nExecuting playbook with third set of variables. Remove exchanges"
 ansible-playbook -i localhost, play.yml -e @vars_3.yml
-docker exec rabbit-mgmt rabbitmqctl list_queues | egrep -v "^amq\." | wc -l | egrep "^\s*1$"
+docker exec rabbit-mgmt rabbitmqctl list_exchanges | egrep -v "^amq\." | wc -l | egrep "^\s*2$"
 echo "\nExchanges removed successfully"
