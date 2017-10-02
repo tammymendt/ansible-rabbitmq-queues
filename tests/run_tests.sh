c5a726116968bc1aef314f949c0a640353745f67
Tamara Mendt <tammymendt@gmail.com>
2017-10-02 16:15:34 +0200

    fix tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,10 +6,10 @@ ansible-playbook -i localhost, play.yml -e @vars_1.yml
 echo "Running rabbit commands on docker to test for rabbit state"
 docker exec rabbit-mgmt rabbitmqctl list_queues | egrep -v "^amq\." | wc -l | egrep "^\s*2$"
 docker exec rabbit-mgmt rabbitmqctl list_exchanges | egrep "some_exchange\s+direct"
-docker exec rabbit-mgmt rabbitmqctl list_queues | egrep "label.deleted\s+"
+docker exec rabbit-mgmt rabbitmqctl list_queues | egrep "some_queue\s+"
 docker exec rabbit-mgmt rabbitmqctl list_queues |  wc -l | egrep "^\s*2$"
-docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "some_exchange\s+exchange\s+label\.deleted\s+queue\s+\*\.labels\.deleted"
-docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "some_exchange\s+exchange\s+label\.deleted\s+queue\s+us\.\*"
+docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "some_exchange\s+exchange\s+some_queue\s+queue\s+\*\.labels"
+docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "some_exchange\s+exchange\s+some_queue\s+queue\s+us\.\*"
 docker exec rabbit-mgmt rabbitmqctl list_bindings
 docker exec rabbit-mgmt rabbitmqctl list_bindings |  wc -l | egrep "^\s*4$"
 
