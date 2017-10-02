dbd9031fdf77290622801a2983f9113b7f43bd7a
Tamara Mendt <tammymendt@gmail.com>
2017-10-02 15:29:32 +0200

    debug

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+set -e
 echo "Executing playbook with first set of variables"
 ansible-playbook -i localhost, play.yml -e @vars_1.yml
 
@@ -10,6 +10,7 @@ docker exec rabbit-mgmt rabbitmqctl list_queues | egrep "label.deleted\s+"
 docker exec rabbit-mgmt rabbitmqctl list_queues |  wc -l | egrep "^\s*2$"
 docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "some_exchange\s+exchange\s+label\.deleted\s+queue\s+\*\.labels\.deleted"
 docker exec rabbit-mgmt rabbitmqctl list_bindings | egrep "some_exchange\s+exchange\s+label\.deleted\s+queue\s+us\.\*"
+docker exec rabbit-mgmt rabbitmqctl list_bindings
 docker exec rabbit-mgmt rabbitmqctl list_bindings |  wc -l | egrep "^\s*3$"
 
 echo "Executing playbook with second set of variables. Remove bindings"
